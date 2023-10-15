5b911ae4af01dc804ebd1160b50fb6f4ed87a7b9
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2023-10-15 03:51:39 +0100

    send form data to gmail test

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const express = require('express') //It imports the Express.js library for build
 const app = express() //It creates an Express application instance called app
 const port = 3005 //It sets the port variable to 3005, which is the port number on which the server will listen.
 const db = require('./connect-db')
-const {Contact, Tableau} = require('./model-doc')
+const postModel = require('./model-doc')
+//npm i cors
 const cors = require('cors')
-const multer = require("multer");
 app.use(cors())
 //It requires two modules: db and model-doc. These likely contain code for establishing a connection to the MongoDB database and defining a Mongoose model, respectively.
 
@@ -17,54 +17,15 @@ app.use(express.json());
 
 //Defining a Route for the Root URL ("/"):
 //This code defines an HTTP GET route for the root URL ("/"). When a client accesses this URL, it sends a simple "Hello World!" message as a response.
-
-app.get('/tableau', async(req, res) =>{
-  try {
-      const posts = await Tableau.find()
-      res.json(posts)
-  } catch (error) {
-      res.status(500).send(error)
-  }
+app.get('/', (req, res) => {
+  res.send('Hello World!')  
 })
-// Set up a storage engine for multer to specify where to store the uploaded images
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/"); // The directory where you want to store uploaded images
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-// Handle POST request for /tableau to upload a new image
-app.post("/tableau", upload.single("thumbnail"), async (req, res) => {
-  try {
-    const { nom } = req.body; // Extract the 'nom' field
-    const thumbnail = req.file; // Uploaded file information
-
-    // Assuming you have a Tableau model with appropriate schema
-    const newTableau = new Tableau({ nom, thumbnail });
-
-    // Save the new tableau data to the database
-    await newTableau.save();
-
-    res.status(201).json({ message: "Tableau record added successfully" });
-  } catch (error) {
-    console.error(error); // Log the error for debugging
-    res.status(500).json({ message: "Error uploading image" }); // Send an error response
-  }
-});
-
-
 
 //Handling POST Requests to the Root URL ("/") after http://localhost:3005 S'il n'y a rien après 3005 (port qu'on a décidé plus haut) on met "/" 
 app.post('/', async(req, res) =>{
-  const {Nom, Prenom, Ville, Mail, Telephone, Aide} = req.body //It extracts the "Nom" and "Prenom" from the request body using destructuring assignment.
+  const {Nom, Prenom} = req.body //It extracts the "Nom" and "Prenom" from the request body using destructuring assignment.
   try{
-    console.log(Telephone);
-      const newPost = await Contact.create({Nom, Prenom, Ville, Mail, Telephone, Aide}); //It attempts to create a new document (record) in the MongoDB collection using the postModel.create() method. 
+      const newPost = await postModel.create({Nom, Prenom}); //It attempts to create a new document (record) in the MongoDB collection using the postModel.create() method. 
       res.json(newPost)//If the document is successfully created, it responds with the newly created document in JSON format.
   }catch(error){
       res.status(500).send(error)
@@ -83,14 +44,3 @@ app.listen(port, () => {
 //!!!!!!!!!!!!!!! Il faut toujours allumer le server avec npx nodemon server.js pour lancer le backend
 
 //In summary, this code sets up an Express.js server that listens on port 3005, provides a simple "Hello World!" message when accessing the root URL with a GET request, and allows the creation of new documents in a MongoDB collection when sending a POST request to the same root URL with JSON data.
-
-
-
-
-
-
-
-
-
-
-

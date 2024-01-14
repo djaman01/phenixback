aefffde48e6a54a127dbb3d9acb6b9573ee78002
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-01-14 01:50:16 +0100

    phenixdeals.onrender

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());//To convert=parse incoming JSON data from HTTP requests
 
 //Objectif: 1) Store le token dans le cookie en front-end side (on l'active grace à une ligne de code dans le component login.js)
 app.use(cors({
-  origin: ["https://phenixback.onrender.com/"],//Local Host: to access the front-end side through this URL
+  origin: ["https://phenixdeals.onrender.com/"],//Local Host: to access the front-end side through this URL
   methods: ["GET", "POST", "PUT", "DELETE"],//
   credentials: true
 }))
